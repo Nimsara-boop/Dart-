5dac76eca37e7c99d1d676854f3db8c25aca9eec
Pugsy <nimsarakarunaratne@gmail.com>
2025-07-21 22:46:21 +0530

    assignment operators ++a and a++

---
+++ compound_assignment_operators.dart
@@ -0,0 +1,13 @@
+ void main() {
+  int a = 10;
+  print(a);
+  print(++a);
+  print(--a);
+  
+  print("-----");
+  
+  int b = 10;
+  print(b);
+  print(b--);
+//   print()
+}
---
+++ first_dart_app/android/app/src/main/kotlin/com/example/first_dart_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.first_dart_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
