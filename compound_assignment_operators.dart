e91d550f08338028872e9a519cdda86f83237d2c
Pugsy <nimsarakarunaratne@gmail.com>
2025-07-22 22:40:05 +0530

    added some assignment operators

--- compound_assignment_operators.dart
+++ compound_assignment_operators.dart
@@ -1,4 +1,7 @@
  void main() {
+
+  print("_________________Incrememnt Decrement operators________________________");
+
   int a = 10;
   print(a);
   print(++a);
@@ -9,5 +12,17 @@
   int b = 10;
   print(b);
   print(b--);
+  print(b);
+  print(b++);
+  print(b);
 //   print()
+
+
+print("_________________Compound Assignment Operators________________________");
+double x= 50;
+print(x+=5);
+print(x-=5);
+print(x*=5);
+print(x/=5);
+
 }
