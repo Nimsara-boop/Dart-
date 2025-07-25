14ac724dc06ac6fcb43f33b64e2dd65e68b8bb69
Pugsy <nimsarakarunaratne@gmail.com>
2025-07-25 17:33:27 +0530

    Arrow Function and Anonymous Function.

---
+++ part5_in_Dart.dart
@@ -0,0 +1,17 @@
+//Arrow function
+
+void main(){
+  //Arrow function => fat arrow... for 1 line functions
+
+  dynamic square(var n) => n*n;  //n*n will be automatically returned
+
+  //Anonymous functions - dart / lambda - Python
+
+  var list = {'apples', 'banana', 'orange'};
+  list.forEach((item){
+  print(item);
+  }
+  );
+
+}
+
